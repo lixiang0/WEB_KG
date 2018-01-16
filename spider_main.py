9d52f16f2c65255e565db0bbaba04e0b2ac20cc9
ruben <xiaozhonghua@yydrobo.com>
2018-01-16 21:00:36 +0800

    4线程爬取

--- spider_main.py
+++ spider_main.py
@@ -1,37 +1,52 @@
-# coding:utf-8
 import url_manager, html_downloader, html_parser
 import pickle
 import os
 import sys
+from multiprocessing.dummy import Pool as ThreadPool
+import threading
 
 
-class SpiderMain(object):
+
+
+
+class MyThread(threading.Thread):
     def __init__(self):
-        self.urls = url_manager.UrlManager()
-        self.downloader = html_downloader.HtmlDownloader()
-        self.parser = html_parser.HtmlParser()
+        threading.Thread.__init__(self)
+    def run(self):
+        while urls.has_new_url():
+            global i
+            lock.acquire()
+            new_url = urls.get_new_url()
+            lock.release()
+            print('craw %d : %s' % (len(urls.old_urls), new_url))
+            html_cont = downloader.download(new_url)
+            new_urls, _ = parser.parse(new_url, html_cont)
+            lock.acquire()
+            urls.add_new_urls(new_urls)
+            lock.release()
 
-    def craw(self, root_url):
-        count = len(self.urls.old_urls)
-        self.urls.add_new_url(root_url)
-        while self.urls.has_new_url():
-            new_url = self.urls.get_new_url()
-            print('craw %d : %s' % (count, new_url))
-            html_cont = self.downloader.download(new_url)
-            # print(html_cont)
-            new_urls, _ = self.parser.parse(new_url, html_cont)
-            self.urls.add_new_urls(new_urls)
-            # print('mark')
-            count = count + 1
 
 if __name__=='__main__':
     root_url = 'http://baike.baidu.com'
-    obj_spider = SpiderMain()
-    if os.path.exists('urls.bin'):
-        obj_spider.urls=pickle.load(open('urls.bin','rb'))
+    lock=threading.Lock()
+    urls = url_manager.UrlManager()
+    downloader = html_downloader.HtmlDownloader()
+    parser = html_parser.HtmlParser()
     try:
-        obj_spider.craw(root_url)
+        if os.path.exists('urls.bin'):
+            urls=pickle.load(open('urls.bin','rb'))
+            MyThread().start()
+            MyThread().start()
+            MyThread().start()
+            MyThread().start()
+        else:
+            urls.add_new_url(root_url)
+            MyThread().start()
+            MyThread().start()
+            MyThread().start()
+            MyThread().start()
     except:
         print('error!', sys.exc_info()[0])
     finally:
-        pickle.dump(obj_spider.urls, open('urls.bin', 'wb'))
+        pickle.dump(urls, open('urls.bin', 'wb'))
+        print('done')
