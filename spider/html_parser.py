3479b8b1fe1fb72675ce989eee5084ce7e7d2f78
Your Name <you@example.com>
2019-06-05 11:46:01 +0800

    优化百科词条的提取方式

--- spider/html_parser.py
+++ spider/html_parser.py
@@ -4,17 +4,24 @@ import re
 from urllib.parse import urljoin
 import os
 import urllib
-
 class HtmlParser(object):
     def _get_new_urls(self, soup):
-        maps = dict()
+        sets = set()
         # /view/123.htm
         #<a target="_blank" href="/item/%E6%9D%8E%C2%B7%E5%A1%94%E7%8E%9B%E9%9C%8D%E7%91%9E/5486870" data-lemmaid="5486870">李·塔玛霍瑞</a>
-        links = soup.find_all('a',href=re.compile('/item/[\u4E00-\u9FA5]+'))
+        links = soup.find_all('a',href=re.compile('/item/[%A-Z\u4E00-\u9FA5]+'))
         for link in links:
-            temp=BeautifulSoup(str(link), 'lxml')
-            maps[temp.find('a').contents[0]]=urljoin('https://baike.baidu.com', temp.find('a')['href']) 
-        return maps
+            temp=BeautifulSoup(str(link), 'lxml').find('a')['href'].replace('https://baike.baidu.com','')
+            result=urllib.parse.unquote(temp)
+            # print(result)
+            result=re.findall('/[！@#￥$%^&*()_+-=·A-Za-z\'.：～:\u4E00-\u9FA50-9]+[/?#]?', result)
+            # print(result)
+            item=result[0].replace('/[0-9]+','').split('/')[2].replace('#hotspotmining','')
+            # print(item)
+            sets.add(urljoin('https://baike.baidu.com/item/',item))
+            # print(urllib.parse.unquote(urljoin('https://baike.baidu.com', '/'.join(temp.find('a')['href'].split('/')[:5]))) )
+            # maps[temp.find('a').contents[0]]=urllib.parse.unquote(urljoin('https://baike.baidu.com', '/'.join(temp.find('a')['href'].split('/')))) 
+        return sets
 
     def _save_new_data(self, soup,html_cont):
         is_saved = False
@@ -23,19 +30,18 @@ class HtmlParser(object):
         path=os.path.join('.','webpages')#custom diectory for webpages
         if not os.path.exists(path):
             os.mkdir(path)
-        with open(os.path.join(path ,title), 'w') as f:
+        with open(os.path.join(path ,title+'.html'), 'w') as f:
             f.write(html_cont.decode('utf-8'))
-            print('Save to disk filename:'+f.name+"")
         return is_saved
 
     def parse(self, html_cont):
         if html_cont is None:
             return
-        soup = BeautifulSoup(html_cont, 'lxml')
-        # print(soup.prettify())
-        maps = self._get_new_urls( soup)
+        soup = BeautifulSoup((html_cont), 'lxml')
+        sets = self._get_new_urls( soup)
+        # print(sets)
         is_saved = self._save_new_data( soup,html_cont)
-        return list(maps.values()), is_saved
+        return sets, is_saved
 
 
 if __name__ == "__main__":
--- spider/spider_main.py
+++ spider/spider_main.py
@@ -3,40 +3,45 @@ import pickle
 import os
 import sys
 import threading
-
-
+import time
+import urllib
 class MyThread(threading.Thread):
-    def __init__(self):
+    def __init__(self,name):
         threading.Thread.__init__(self)
         self._running = True
+        self.name=name
+        # print(self.name)
     def terminate(self):
         self._running = False
     def run(self):
         try:
             while urls.has_new_url() and self._running:
+                start=time.time()
                 LOCK.acquire()
                 new_url = urls.get_new_url()
                 LOCK.release()
-                print('craw %d' % (len(urls.old_urls)),new_url)
                 html_cont = downloader.download(new_url)
                 new_urls, _ = parser.parse(html_cont)
                 LOCK.acquire()
                 urls.add_new_urls(new_urls)
+                spend=time.time()-start
                 LOCK.release()
+                print(f"Thread:{self.name} craw id:{len(urls.old_urls)} URL:{urllib.parse.unquote(new_url).split('/')[-1]} spend:{str(spend)}")
         except:
             print('save state',sys.exc_info())
             pickle.dump(urls, open('urls.bin', 'wb'))
 
 
 if __name__=='__main__':
+
     PATH='urls.pkl'
-    root_url = 'http://baike.baidu.com'
+    root_url = 'https://baike.baidu.com/item/%E6%96%87%E6%B1%87%E6%8A%A5'
     LOCK=threading.Lock()
     urls = url_manager.UrlManager()
     downloader = html_downloader.HtmlDownloader()
     parser = html_parser.HtmlParser()
     threads=[]
-    count_thread=36
+    count_thread=12
     if os.path.exists(PATH):
         urls=pickle.load(open(PATH,'rb'))
     else:
@@ -45,7 +50,7 @@ if __name__=='__main__':
     print(f'build urls,length={length}')
     for i in range(count_thread):
         print(f'build thread {i}...')
-        threads.append(MyThread())
+        threads.append(MyThread(str(i)))
     try:
         for t in threads:
             t.start()
